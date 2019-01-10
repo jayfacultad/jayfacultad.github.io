0721a88193c166826b27dcf2b8dd2ea379220287
Jay Facultad <jayfacultad@gmail.com>
2019-01-10 17:20:44 -0500

    correction in parenthesis

--- graph.js
+++ graph.js
@@ -9,7 +9,7 @@ function Graph(num_vertices) {
     this.vertices = new Array(num_vertices).fill(null);
     this.edges = new Array(num_vertices).fill(null);
     for (var i = 0; i < Math.sqrt(num_vertices); i++) {
-        this.edges[i] = new Array(Math.sqrt(num_vertices).fill(null));
+        this.edges[i] = new Array(Math.sqrt(num_vertices)).fill(null);
     }
     this.path = new Array(num_vertices).fill(null);
     this.marks = new Array(num_vertices).fill(null);
