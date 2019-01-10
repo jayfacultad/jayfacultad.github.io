cfdaeea6179b9a04e9ecaea0c6c28d38e2ed8d73
Jay Facultad <jayfacultad@gmail.com>
2019-01-10 17:49:48 -0500

    corrected sizes of edges arrays

--- boggleinitiator.js
+++ boggleinitiator.js
@@ -137,8 +137,8 @@ function create_dictionary() {
 function create_boggle_graph() {
 
     // Number of rows and columns
-    var row = 5;
-    var col = 5;
+    var row = Math.sqrt(num_vertices);
+    var col = Math.sqrt(num_vertices);
 
     // Positions relative to the current tile.  These variables will be used
     // to determine neighbors.  Once neighbors are determined, the proper 
