21898dce5c38011badfb44b3d19c22e4f6b8742c
Jay Facultad <jayfacultad@gmail.com>
2019-01-12 18:48:30 -0500

    removed console log tests

--- boggleinitiator.js
+++ boggleinitiator.js
@@ -193,14 +193,6 @@ function create_boggle_graph() {
             }
         }
     }
-
-    // Test boggle graph:
-    console.log("Test1");
-    for (var i = 0; i < num_of_tiles; i++) {
-        for (var j = 0; j < num_of_tiles; j++) {
-            boggle_graph.is_edge(i, j);
-        }
-    }
 }
 
 
