c58370742056d32cd1703e3c1de3fb682b813d58
Jay Facultad <jayfacultad@gmail.com>
2022-01-21 21:45:39 -0500

    Update homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -1,11 +1,13 @@
 var mobileSize = 480;
 var tabletSize = 768;
 
-setDesktopOrMobileView();
+window.addEventListener('DOMContentLoaded', (event) => {
+  setDesktopOrMobileView();
+});
 
 function setDesktopOrMobileView() {
   var width = window.innerWidth;
-  if(width <= mobileSize) {
+  if(width <= mobileSize && document.getElementById("hero-image") != null) {
     document.getElementById("hero-image").src = "/staticresources/newyorkmobile.jpg";
   } else {
     document.getElementById("hero-image").src = "/staticresources/newyork.jpg";
