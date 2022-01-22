6056f72f9f6c77e6d04b19c3762c459508c6625f
Jay Facultad <jayfacultad@gmail.com>
2022-01-22 15:42:06 -0500

    Update homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -7,22 +7,8 @@ window.addEventListener('load', (event) => {
 
 function setDesktopOrMobileView() {
   var width = window.innerWidth;
-  if(window.innerWidth <= mobileSize || window.outerWidth <= mobileSize) {
+  if(window.innerWidth <= mobileSize ) {
     document.getElementById("hero-image").src = "/staticresources/newyorkmobile.jpg";
-    document.getElementById("intro-block").classList.remove("fadeInUp");
-    document.getElementById("intro-block").classList.add("fadeInUp-mobile");
-    document.getElementById("heading").classList.remove("heading");
-    document.getElementById("heading").classList.add("heading-mobile");
-    document.getElementById("eyebrow").classList.remove("eyebrow");
-    document.getElementById("eyebrow").classList.add("eyebrow-mobile");
-    document.getElementById("title").classList.remove("title");
-    document.getElementById("title").classList.add("title-mobile");
-    document.getElementById("hero-image").classList.remove("hero-image");
-    document.getElementById("hero-image").classList.add("hero-image-mobile");
-    document.getElementById("scroll-down-container").classList.remove("scroll-down-container");
-    document.getElementById("scroll-down-container").classList.add("scroll-down-container-mobile");
-    document.getElementById("scroll-down-icon").classList.remove("scroll-down-icon");
-    document.getElementById("scroll-down-icon").classList.add("scroll-down-icon-mobile");
   } 
   
 }
