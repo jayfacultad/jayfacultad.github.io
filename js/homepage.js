710e611dff7335d932833d8adc56d2062dd82ba4
Jay Facultad <jayfacultad@gmail.com>
2022-01-21 22:47:16 -0500

    Update homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -9,12 +9,12 @@ function setDesktopOrMobileView() {
   var width = window.innerWidth;
   if(window.innerWidth <= mobileSize || window.outerWidth <= mobileSize) {
     document.getElementById("hero-image").src = "/staticresources/newyorkmobile.jpg";
-    document.getElementById("hero-image").classList.remove("fadeInUp");
-    document.getElementById("hero-image").classList.add("fadeInUp-mobile");
-    document.getElementById("hero-image").classList.remove("eyebrow");
-    document.getElementById("hero-image").classList.add("eyebrow-mobile");
-    document.getElementById("hero-image").classList.remove("title");
-    document.getElementById("hero-image").classList.add("title-mobile");
+    document.getElementById("intro-block").classList.remove("fadeInUp");
+    document.getElementById("intro-block").classList.add("fadeInUp-mobile");
+    document.getElementById("eyebrow").classList.remove("eyebrow");
+    document.getElementById("eyebrow").classList.add("eyebrow-mobile");
+    document.getElementById("title").classList.remove("title");
+    document.getElementById("title").classList.add("title-mobile");
   } 
   
 }
