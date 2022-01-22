574e529177502b2a0bba349c40bacb5d47f9b67a
Jay Facultad <jayfacultad@gmail.com>
2022-01-21 21:24:24 -0500

    Create homepage.js

---
+++ js/homepage.js
@@ -0,0 +1,14 @@
+var mobileSize = 480;
+var tabletSize = 768;
+
+setDesktopOrMobileView();
+
+function setDesktopOrMobileView() {
+  var width = window.innerWidth;
+  if(w <= mobileSize) {
+    document.getElementById("hero-image").src = "/staticresources/newyorkmobile.jpg";
+  } else {
+    document.getElementById("hero-image").src = "/staticresources/newyork.jpg";
+  }
+  
+}
