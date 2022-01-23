eee50c070f8c57e226f1ec591b3f9a98ce222d3f
Jay Facultad <jayfacultad@gmail.com>
2022-01-23 09:20:07 -0500

    Update homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -1,8 +1,13 @@
 const mobileSize = 480;
 const tabletSize = 768;
 
-(function () {
+window.addEventListener('load', (event) => {
+  setDesktopOrMobileView();
+});
+
+function setDesktopOrMobileView() {
   if(window.screen.width <= mobileSize ) {
     document.getElementById("hero-image").src = "/staticresources/newyorkmobile.jpg";
   } 
-})();
+  
+}
