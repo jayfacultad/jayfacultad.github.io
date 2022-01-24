7fcef98d94afa1c523a78e0b8bd619f07675e95f
Jay Facultad <jayfacultad@gmail.com>
2022-01-23 20:52:12 -0500

    Update homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -31,18 +31,15 @@ function fadeOutOnScroll(element) {
   
   console.log('--------------------------------------------');
   
-  console.log('element.getBoundingClientRect().top ' + element.getBoundingClientRect().top);
-  console.log('document.documentElement.scrollTop ' + document.documentElement.scrollTop);
-  
-  console.log('----');
+  console.log('window.pageYOffset + element.getBoundingClientRect().top');
+  console.log(window.pageYOffset + ' + ' + element.getBoundingClientRect().top));
+  console.log('elementHeight ' + elementHeight);
+  console.log('window.pageYOffset ? window.pageYOffset : document.documentElement.scrollTop');
+  console.log(window.pageYOffset + ' ? ' + window.pageYOffset + ' : ' + document.documentElement.scrollTop');
   
   var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
   var elementHeight = element.offsetHeight;
   var scrollTop = window.pageYOffset ? window.pageYOffset : document.documentElement.scrollTop;
-  
-  console.log('distanceToTop ' + distanceToTop);
-  console.log('elementHeight ' + elementHeight);
-  console.log('scrollTop ' + scrollTop);
   
   console.log('--------------------------------------------');
   
